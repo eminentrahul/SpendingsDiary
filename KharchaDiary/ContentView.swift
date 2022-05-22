b6de7c1b8b9455733bd55c88947b99c31b76c52d
Rahul Ravi Prakash <rahul@thebrewapps.com>
2022-05-22 16:39:15 +0530

    Initial Commit

---
+++ KharchaDiary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KharchaDiary
+//
+//  Created by Rahul Ravi Prakash on 22/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KharchaDiaryUITests/KharchaDiaryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  KharchaDiaryUITestsLaunchTests.swift
+//  KharchaDiaryUITests
+//
+//  Created by Rahul Ravi Prakash on 22/05/22.
+//
+
+import XCTest
+
+class KharchaDiaryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
